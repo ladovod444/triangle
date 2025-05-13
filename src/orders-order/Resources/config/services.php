43af176a2f74f9f126583e21af409ca521a822c1
dz <ladovod@gmail.com>
2025-05-13 15:10:46 +0500

    Правки по карточкам в списках, правки карусели на странице товара

---
+++ src/orders-order/Resources/config/services.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function(ContainerConfigurator $configurator): void {
+
+    $services = $configurator
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $NAMESPACE = 'Orders\\Order\\';
+
+    $MODULE = substr(__DIR__, 0, strpos(__DIR__, "Resources"));
+
+    $services
+        ->load($NAMESPACE, $MODULE)
+        ->exclude($MODULE.'{Entity,Resources,Type,*DTO.php,*Message.php}');
+
+};
