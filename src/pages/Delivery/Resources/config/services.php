b8862558c66242cb1997beb0e073871a043ddf25
dz <ladovod@gmail.com>
2025-05-15 21:39:30 +0500

    Перемешает шаблоны для страниц доставки и контактов в src, неиспользуемые в папку templates/old

---
+++ src/pages/Delivery/Resources/config/services.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function(ContainerConfigurator $configurator): void {
+
+    $services = $configurator
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $NAMESPACE = 'Pages\\Delivery\\';
+
+    $MODULE = substr(__DIR__, 0, strpos(__DIR__, "Resources"));
+
+    $services
+        ->load($NAMESPACE, $MODULE)
+        ->exclude($MODULE.'{Entity,Resources,Type,*DTO.php,*Message.php}');
+};
